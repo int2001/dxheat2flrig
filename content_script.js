3feeb73b056031530a7fc88b24d4fb0684e9d833
Jörg D <Joerg int2001@users.noreply.github.com>
2023-01-15 09:55:30 +0100

    Firefox-Version with pota.app integration

--- content_script.js
+++ content_script.js
@@ -1,9 +1,14 @@
 document.addEventListener('click', function (event) {
 
-    if (!event.target.matches('.frequency')) return;
-
-    event.preventDefault();
-    let qrg = event.target.innerText.replace(/[\s|,]/g, '') * 100;
-    chrome.runtime.sendMessage({"message": "setVfo", "qrg": qrg});
-
+	if (event.target.matches('.frequency')) {
+		let call=event.target.parentNode.cells[3].textContent;
+		event.preventDefault();
+		let qrg = event.target.innerText.replace(/[\s|,]/g, '') * 100;
+		chrome.runtime.sendMessage({"message": "setVfo", "qrg": qrg, "call":call});
+	} else if (event.target.innerText.match(/.*kHz.*/g)) {
+		let qrg=event.target.innerText.replace(/(\d+).*$/g,"$1")*1000;
+		chrome.runtime.sendMessage({"message": "setVfo", "qrg": qrg});
+	} else {
+		return;
+	}
 }, false);
